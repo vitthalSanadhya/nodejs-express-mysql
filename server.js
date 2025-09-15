6c3b3566f9c106deb99b5def73cfd7c48a0c7b8d
Vitthal Sanadhya <77016445+vitthalSanadhya@users.noreply.github.com>
2025-09-15 17:49:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is d
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder applicationtes hello." });
+  res.json({ message: "Welcome to bezkoder applicationtes hccccccello." });
 });
 
 require("./app/routes/tutorial.routes.js")(app);
